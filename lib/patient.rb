d1b2d8ac42f11145092f6a1d01eaa53c81aeb952
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-19 00:35:43 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+  def initialize(genre)
+    @name = genre
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    songs.map {|song| song.artist }
+  end
+end
