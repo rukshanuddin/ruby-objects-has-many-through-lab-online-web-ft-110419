01a54576edea1bae0556f5a8e17b30cf73285713
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-19 00:29:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,27 @@
 class Doctor
 
-  attr_accessor
+  attr_accessor :name
 
   @@all = []
 
-  def initalize(name)
+  def initialize(name)
     @name = name
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    songs.map {|song| song.genre}
   end
 end
